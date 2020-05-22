9de497160dabe1a96ab0b4a48cab8fda8d398113
Angeljcc102 <angeljcc102@outlook.es>
2020-05-22 17:02:42 -0500

    Changed GUIFrame properties and added getters

--- src/producerconsumer/ProducerConsumer.java
+++ src/producerconsumer/ProducerConsumer.java
@@ -5,6 +5,12 @@ public class ProducerConsumer {
 
     public static void main(String[] args) {
         
+        GUIFrame frame = new GUIFrame();
+        frame.setLocationRelativeTo(null);
+        frame.setResizable(false);
+        frame.pack();
+        frame.setVisible(true);
+        
         Buffer buffer = new Buffer();
         
         Producer producer = new Producer(buffer);
