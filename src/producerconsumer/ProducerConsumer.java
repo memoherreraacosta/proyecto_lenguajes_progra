653a97119577f246bd06273f24740bd6d6826340
Angeljcc102 <angeljcc102@outlook.es>
2020-05-23 07:58:24 -0500

    changed Consumer constructor and added function to Producer

--- src/producerconsumer/ProducerConsumer.java
+++ src/producerconsumer/ProducerConsumer.java
@@ -5,16 +5,19 @@ public class ProducerConsumer {
 
     public static void main(String[] args) {
         
+        
         GUIFrame frame = new GUIFrame();
         frame.setLocationRelativeTo(null);
         frame.setResizable(false);
         frame.pack();
         frame.setVisible(true);
         
-        Buffer buffer = new Buffer();
         
-        Producer producer = new Producer(buffer);
-        Consumer consumer = new Consumer(buffer);
+        Buffer buffer = new Buffer();
+        int timeout = 1000;
+        
+        Producer producer = new Producer(buffer, timeout);
+        Consumer consumer = new Consumer(buffer, timeout);
         
         producer.start();
         consumer.start();
